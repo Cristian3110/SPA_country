6742d56179f6604528401a64b217d21f7f0f1921
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2023-05-25 23:27:22 -0400

    doing conexion with RestAPI countryRest, creating conexion with Rest and Services Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
@@ -8,7 +9,13 @@ import { AppRoutinModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutinModule, PaisModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutinModule,
+    HttpClientModule,
+    PaisModule,
+    SharedModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
