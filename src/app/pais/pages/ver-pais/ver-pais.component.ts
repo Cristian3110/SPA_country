5271cc8c0ffc03fe13a1d4cbd383a3f75579a38c
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2023-05-30 20:34:05 -0400

    configuration with the operator switchMap to extract information about route

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PaisService } from '../../services/pais.service';
 
+import { switchMap } from 'rxjs/operators';
+
 @Component({
   selector: 'app-ver-pais',
   templateUrl: './ver-pais.component.html',
@@ -14,15 +16,21 @@ export class VerPaisComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    //with operators SwitchMap
+    this.activedRoute.params
+      .pipe(switchMap(({ id }) => this.paisService.getPaisPorAlpha(id)))
+
+      .subscribe((resp) => {
+        console.log(resp);
+      });
+    // utilizando el operador SwitchMap
     // inicialilzando el component
     // desestructurando los params extrayendo el Id
-
-    this.activedRoute.params.subscribe(({ id }) => {
-      console.log(id);
-
-      this.paisService.getPaisPorAlpha(id).subscribe((pais) => {
-        console.log(pais);
-      });
-    });
+    // this.activedRoute.params.subscribe(({ id }) => {
+    //   console.log(id);
+    //   this.paisService.getPaisPorAlpha(id).subscribe((pais) => {
+    //     console.log(pais);
+    //   });
+    // });
   }
 }
