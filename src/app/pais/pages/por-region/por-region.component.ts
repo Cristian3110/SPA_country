bddada74366cd675383e4aa2a99a08c03834851c
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2023-06-07 03:10:07 -0400

    configuration of he class of CSS adding Class condictionals of CSS from components TS

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
-  styleUrls: ['./por-region.component.css']
+  styleUrls: ['./por-region.component.css'],
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent {
+  regiones: string[] = ['africa', 'americas', 'europe', 'asia', 'oceania'];
+  regionActiva: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  //evaluando la clase de Css y enviandola al DOM
+  getClaseCSS(region: string): string {
+    return region === this.regionActiva
+      ? 'btn btn-outline-primary'
+      : 'btn btn-outline-primary';
   }
 
+  activarRegion(region: string) {
+    this.regionActiva = region;
+  }
 }
